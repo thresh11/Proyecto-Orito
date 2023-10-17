832ad0035fb86da1ee33d246eefd2b37fbc3122b
jhon_snow <jhonjairoverasaavedra4@gmail.com>
2023-10-16 22:05:17 -0500

    introduccion de los productos

---
+++ productos/scrit_productos.js
@@ -0,0 +1,65 @@
+window.addEventListener('scroll', function(){
+    var contenedor_barra_navegadora = document.querySelector(".navegador");
+    contenedor_barra_navegadora.classList.toggle("navegadorAbajo", window.scrollY>0)
+  })
+
+
+window.addEventListener('scroll', function () {
+    var cambiarColorLetra = document.querySelectorAll("header nav ul li a");
+    cambiarColorLetra.forEach(function (elemento) {
+        elemento.classList.toggle("aabajo", window.scrollY > 0);    
+    });   
+});
+
+
+
+const todosRadio = document.getElementById("todosRadio");
+const brauniRadio = document.getElementById("brauniRadio");
+const comboRadio = document.getElementById("comboRadio");
+const ofertaRadio = document.getElementById("ofertaRadio");
+
+const contenedores = document.querySelectorAll(".pro__contedor__caja");
+
+todosRadio.addEventListener("change", () => {
+    if (todosRadio.checked) {
+        filterBoxes("todos");
+    }
+});
+
+brauniRadio.addEventListener("change", () => {
+    if (brauniRadio.checked) {
+        filterBoxes("brauni");
+    }
+});
+
+comboRadio.addEventListener("change", () => {
+    if (comboRadio.checked) {
+        filterBoxes("combo");
+    }
+});
+
+ofertaRadio.addEventListener("change", () => {
+    if (ofertaRadio.checked) {
+        filterBoxes("oferta");
+    }
+});
+
+function filterBoxes(categoria) {
+    contenedores.forEach((pro__contedor__caja) => {
+        if (categoria === "todos" || pro__contedor__caja.classList.contains(categoria)) {
+            pro__contedor__caja.style.display = "block";
+        } else {
+            pro__contedor__caja.style.display = "none";
+        }
+    });
+}
+
+
+
+  function cambiarImagen(imagen) {
+            imagen.src = "../img/almendras.jpg";
+        }
+
+        function restaurarImagen(imagen) {
+            imagen.src = "../img/brawni_almendras2-removebg-preview1.png";
+        }
