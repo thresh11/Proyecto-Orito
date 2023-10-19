b0e4808b76781ddeed7fa6dcd29d85d5a1b19aa5
thresh11 <jhonjairoverasaavedra4@gmail.com>
2023-10-19 11:41:04 -0500

    da

--- productos/scrit_productos.js
+++ productos/scrit_productos.js
@@ -63,3 +63,41 @@ function filterBoxes(categoria) {
         function restaurarImagen(imagen) {
             imagen.src = "../img/brawni_almendras2-removebg-preview1.png";
         }
+
+
+
+
+
+        // DESLIZAR
+
+
+        const contenedor = document.getElementById('pro__contedor__busqueda');
+        const cajas = document.getElementById('pro__contedor__caja');
+        let desplazamiento = 0;
+
+        function deslizarArriba() {
+            if (desplazamiento < 0) {
+                desplazamiento += 60;
+                cajas.style.top = desplazamiento + 'px';
+            }
+        }
+
+        function deslizarAbajo() {
+            const alturaContenedor = contenedor.clientHeight;
+            const alturaCajas = cajas.clientHeight;
+
+            if (alturaCajas + desplazamiento > alturaContenedor) {
+                desplazamiento -= 60;
+                cajas.style.top = desplazamiento + 'px';
+            }
+        }
+
+        window.addEventListener('load', () => {
+            contenedor.addEventListener('wheel', (event) => {
+                if (event.deltaY < 0) {
+                    deslizarArriba();
+                } else {
+                    deslizarAbajo();
+                }
+            });
+        });
