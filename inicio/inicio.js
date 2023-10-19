e6ecfb5e46f5a05e3ff8b65497c7246e7b77097a
Aprendiz <Aprendiz@IBAAPRTLFSP165.FormacionSena.RED>
2023-10-19 06:31:02 -0500

    soy el jefe #1

--- inicio/inicio.js
+++ inicio/inicio.js
@@ -127,3 +127,33 @@ function loadHtml() {
 function clearHtml() {
     containerBuyCart.innerHTML = '';
 }
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+    autoplay: {
+        delay: 6000,
+        pauseOnMouseEnter: true,
+        disableOnInteraction: false,
+    },
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+  
+        // dynamicBullets: true
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    // scrollbar: {
+    //   el: '.swiper-scrollbar',
+    // },
+  });
